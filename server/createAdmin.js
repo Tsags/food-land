56b2cadae3bb9de12f7e5941350148d6a2851c92
Tsags <66217536+Tsags@users.noreply.github.com>
2026-04-22 14:29:54 +0300

    Update createAdmin.js for hashed password

--- server/createAdmin.js
+++ server/createAdmin.js
@@ -1,13 +1,18 @@
 import User from "./models/User.js";
+import bcrypt from "bcrypt";
 
 export async function createAdminUser() {
-  const exists = await User.findOne({ name: "mayonko" });
+  const exists = await User.findOne({ name: "admin" });
   if (!exists) {
-    await User.create({
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash("admin", salt);
+    await User.collection.insertOne({
       name: "admin",
-      password: "admin",
+      password: hashedPassword,
       isAdmin: true,
       qrCodeData: "admin",
+      createdAt: new Date(),
+      updatedAt: new Date(),
     });
     console.log("Admin user created!");
   }
